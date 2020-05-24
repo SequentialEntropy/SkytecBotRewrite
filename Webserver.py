20c4dd8855c6ba545e293b001a6cee7de0b17ace
Shadow711 <asahi79289@gmail.com>
2020-05-24 09:11:00 +0800

    Edited webserver shutting down method to RuntimeError

--- Webserver.py
+++ Webserver.py
@@ -3,26 +3,24 @@ from threading import Thread
 
 app = Flask(__name__)
 
-kill = False
-
 @app.route("/")
 def main():
     return "Bot is running."
 
 def server():
     print("Server code running.")
-    global kill
     server = Thread(target=run)
     server.start()
-    while not kill:
-        print("Webserver Alive.")
-    print("Webserver Dying.")
 
 def kill():
     print("Proceeding to kill webserver.")
-    global kill
-    kill = True
+    raise RuntimeError("Shutting down server")
+    print("Shutting down server, RuntimeError raised")
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
-    print("Webserver is ready.")
+    try:
+        app.run(host="0.0.0.0", port=8080)
+        print("Webserver is ready.")
+    except RuntimeError, msg:
+        if str(msg) == "Shutting down server":
+            print("Shutting down server, RuntimeError caught")
